d0e4cd7ff85bb1789d490b0484666d22fc186ab6
Weishen <weishenzou@gmail.com>
2023-11-16 14:49:49 +0100

    detai without Statement

--- firstTry/ts2uml/lib/parser.js
+++ firstTry/ts2uml/lib/parser.js
@@ -22,11 +22,10 @@ class TSNode {
             map[this.name] = this.children.length
                 ? this.children
                       .map(child => child.getObject())
-                      .reduce((pv, child) => {          //union tout les element,
+                      .reduce((pv, child) => {          //union tout les element
                           for (let key in child) {
                               if (pv.hasOwnProperty(key) || key in pv) {
                                   if (key != "Number" && key != "Date"){//assign direct
-                                    // console.log("key:"+key);
                                     Object.assign(pv[key], child[key]);
                                   }else {
                                     pv[key] = child[key];//union element
@@ -52,8 +51,9 @@ let visit = parent => node => {
     //if(customStringify(node)){console.log("***");console.log(customStringify(node));console.log("***");}
     let test = customStringify(node);       //toString
     if (test && test.includes("Statement")) {       //if contient "Statement"
+        //console.log(test);
         if (ts.isInterfaceDeclaration(node)){       //if type is interfaceDeclaration
-            let m1 = node.members[1]                //get node's info
+            //let m1 = node.members[1]                //get node's info
             //console.log("######"+m1.type.typeName.escapedText+ "----" + m1.type.typeArguments[0].typeName.escapedText);
            // ts.forEachChild(node, console.log(node.pos))
         }
@@ -70,8 +70,8 @@ let visit = parent => node => {
             ts.forEachChild(node, visit(parent));
             break;
         case ts.SyntaxKind.InterfaceDeclaration:
-            
-            let interfaceName = node.name.text;
+            let interfance = node 
+            let interfaceName = node.name.text+" <interface>";
             parent[interfaceName] = {};
             ts.forEachChild(node, visit(parent.addChildren(interfaceName)));
             break;
@@ -89,29 +89,19 @@ let visit = parent => node => {
                 propertyType = propertyType.elementType;
             }
             if (propertyType.kind === ts.SyntaxKind.TypeReference) {
-                //console.log('tr '+ JSON.stringify(propertyType));
-                
                 let realPropertyType = propertyType;
                 parent.addChildren(
                     realPropertyName,
                     'Array<'.repeat(arrayDeep) +
                         (realPropertyType.kind === ts.SyntaxKind.QualifiedName
-                            //? realPropertyType.getText()+"toto"
-                            ? realPropertyType.typeName.text + "toto"
-                            /*: ! 'text' in realPropertyType
-                              ? realPropertyType +"zaza"
-                              : realPropertyType.typeArguments?.length > 0 
-                                ? realPropertyType.text+"<"+realPropertyType?.typeArguments[0]?.type.typeName+">"
-                                : "ici"
-                                */
+                            ? realPropertyType.typeName.text
                             : 'text' in realPropertyType
-                                ? realPropertyType.typeName.text +"zaza1"     
+                                ? realPropertyType.typeName.text    
                                 : realPropertyType.typeArguments?.length > 0 
-                                    //? realPropertyType.type.text + "<" + realPropertyType?.typeArguments[0]?.type.typeName + ">"
                                     ? realPropertyType.typeName.text + "<" + realPropertyType.typeArguments[0].typeName.text + ">"
-                                    : realPropertyType.typeName.text
+                                    : realPropertyType.typeName.text + "<" + realPropertyType.typeName.constructor.name + ">"
                         ) + 
-                        '>'.repeat(arrayDeep)
+                    '>'.repeat(arrayDeep)
                 );
             } else {
                 for (let type in  PROPERTY_TYPES) {
@@ -130,9 +120,6 @@ let visit = parent => node => {
 module.exports = function(filename, options) {
     const ROOT_NAME = 'root';
     const node = new TSNode(ROOT_NAME);
-    //console.log(node.name);
-    //console.log(node.type);
-
     let program = ts.createProgram([filename], options);
     // let checker = program.getTypeChecker();
     // checker.runWithCancellationToken()
@@ -141,9 +128,7 @@ module.exports = function(filename, options) {
         if (f.fileName == filename){
             let sourceFile = f; 
             ts.forEachChild(sourceFile, visit(node));
-            console.log("zaza")
             console.log(node.getObject()[ROOT_NAME]);
-            console.log("zaza")
         }
     })
     // let sourceFile = program.getSourceFiles()[1];
@@ -156,19 +141,18 @@ module.exports = function(filename, options) {
 function customStringify(obj, seen = new Set()) {
     if (typeof obj === 'object' && obj !== null) {
       if (seen.has(obj)) {
-        // Si l'objet a déjà été vu, affichez un message pour indiquer la référence circulaire.
+        // Check objet has been seen ? message "circular Reference".
         return '[Circular Reference]';
       }
   
-      // Ajoutez cet objet à l'ensemble des objets vus.
       seen.add(obj);
 
       if (Array.isArray(obj)) {
-        // Si c'est un tableau, récursivement stringify chaque élément.
+        // It's Array, stringify for each element.
         const arrayContents = obj.map(item => customStringify(item, seen));
         return `[${arrayContents.join(', ')}]`;
       } else {
-        // Si c'est un objet, récursivement stringify chaque propriété.
+        // It's object, stringify for each proprity.
         const objectContents = Object.keys(obj).map(key => {
           const value = customStringify(obj[key], seen);
           return `${key}: ${value}`;
