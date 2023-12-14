58bb17a53875f91b579bd98c34b7d93ca47b8a8e
weishen <weishenzou@gmail.com>
2023-12-14 10:21:03 +0100

    parser & topu

--- firstTry/ts2uml/lib/parser.js
+++ firstTry/ts2uml/lib/parser.js
@@ -1,6 +1,7 @@
 'use strict';
 const ts = require('typescript');
-const fs = require('fs') 
+const fs = require('fs')
+const toPUml = require('./toUML/topu.js');
 const PROPERTY_TYPES = {
     any: ts.SyntaxKind.AnyKeyword,
     boolean: ts.SyntaxKind.BooleanKeyword,
@@ -118,7 +119,7 @@ module.exports = function getNode(filename, options) {
             ts.forEachChild(sourceFile, visit(node));
             const root  = node.getObject()[ROOT_NAME];
             //console.log(root);
-            let res = toPUml(node);
+            let res = toPUml(node);/********************/
             fs.writeFile(filename+".puml", res, (err) => { 
                 if(err) { 
                     throw err; 
@@ -182,85 +183,3 @@ function customStringify(obj, seen = new Set()) {
 }
 
 
-
-function isClassName(node,name){
-    return node.children.some(element => element.name === name);
-}
-
-function isCContainer(node, cName) {
-    for (let i = 0; i < node.children.length; i++) {
-        const element = node.children[i];
-        if (element.name === cName) {
-            for (let j = 0; j < element.children.length; j++) {
-                if (element.children[j].name == "$container") {
-                    return true;
-                }
-            }
-        }
-    }
-    return false;
-}
-
-function isContainer(node,elem,c){
-    let result="";
-    node.children.forEach(element => {
-        if (!isCContainer(node, c) && element.name != elem.name) {
-            result = "*";
-        }
-    });
-
-    return result;
-}
-
-function toPUml(node) {//node.name="root"
-    var resultat="";
-    for (let i=0; i < node.children.length; i++){
-        const elem = node.children[i];
-        resultat += "class "+ elem.name + "{\n";
-        let str= selectAttribute(node,elem);
-        resultat += str[0];
-        resultat += "}\n";
-        resultat += str[1];
-    }
-    //console.log(resultat);
-    return resultat;
-}
-
-function selectAttribute(node,elem){
-    var str="";
-    var attribute="";
-    var lastName=[];
-    for (let i=0; i < elem.children.length; i++){
-        const c = elem.children[i];
-        while(lastName.length !=0){lastName.pop();}
-        if(c.type != undefined){
-            if(c.type.includes('Array<')){
-                let exp = /<([^>]+)>/;
-                let match = exp.exec(c.type);
-                str += elem.name + isContainer(node,elem,c) + "-->" + "\"" + c.name + "\\n0...*" + "\"" + match[1] +"\n";
-            }
-            else if(c.type.includes('Reference<')){
-                let exp = /<([^>]+)>/;
-                let match = exp.exec(c.type);
-                str += elem.name + "--> " + "\"" + c.name + "\\n0...1" + "\"" + match[1] + "\n";
-            }else if(isClassName(node,c.type)){
-                if(c.name != "$container"){
-                    str += elem.name + "*--> " + "\"" + c.name  + "\\n1 " + "\"" + c.type + "\n";
-                }
-            }
-            else{
-                attribute += c.name + ":" + c.type + "\n";
-            }
-        }
-        else{
-            if(lastName.length != 0){
-                str + lastName[0] + "-[hidden]>" + c.name;
-                lastName.pop();
-            }else{
-                lastName.push(c.name);
-            }
-            str += elem.name+ " <|-- "+ c.name+"\n";
-        }
-    }
-    return [attribute,str];
-}
--- firstTry/ts2uml/lib/toUML/topu.js
+++ firstTry/ts2uml/lib/toUML/topu.js
@@ -1,15 +1,83 @@
-function toPUml(node){
-    for (let i=0; i < node.children.length; i++){
-        const elem = node.children[i]
-        console.log("class "+elem.name+"{\n}");
-        for (let j=0; j < elem.children.length; j++){
-            const c = elem.children[j]
-            if(c.type != undefined){
-                console.log(elem.name, " --> ",c.name, c.type)    
-            }else{
-                console.log(elem.name, " <|-- ", c.name)
-    
+function isClassName(node,name){
+    return node.children.some(element => element.name === name);
+}
+
+function isCContainer(node, cName) {
+    for (let i = 0; i < node.children.length; i++) {
+        const element = node.children[i];
+        if (element.name === cName) {
+            for (let j = 0; j < element.children.length; j++) {
+                if (element.children[j].name == "$container") {
+                    return true;
+                }
             }
         }
     }
+    return false;
 }
+
+function isContainer(node,elem,c){
+    let result="";
+    node.children.forEach(element => {
+        if (!isCContainer(node, c) && element.name != elem.name) {
+            result = "*";
+        }
+    });
+
+    return result;
+}
+
+function toPUml(node) {//node.name="root"
+    var resultat="";
+    for (let i=0; i < node.children.length; i++){
+        const elem = node.children[i];
+        resultat += "class "+ elem.name + "{\n";
+        let str= selectAttribute(node,elem);
+        resultat += str[0];
+        resultat += "}\n";
+        resultat += str[1];
+    }
+    //console.log(resultat);
+    return resultat;
+}
+
+function selectAttribute(node,elem){
+    var str="";
+    var attribute="";
+    var lastName=[];
+    for (let i=0; i < elem.children.length; i++){
+        const c = elem.children[i];
+        while(lastName.length !=0){lastName.pop();}
+        if(c.type != undefined){
+            if(c.type.includes('Array<')){
+                let exp = /<([^>]+)>/;
+                let match = exp.exec(c.type);
+                str += elem.name + isContainer(node,elem,c) + "-->" + "\"" + c.name + "\\n0...*" + "\"" + match[1] +"\n";
+            }
+            else if(c.type.includes('Reference<')){
+                let exp = /<([^>]+)>/;
+                let match = exp.exec(c.type);
+                str += elem.name + "--> " + "\"" + c.name + "\\n0...1" + "\"" + match[1] + "\n";
+            }else if(isClassName(node,c.type)){
+                if(c.name != "$container"){
+                    str += elem.name + "*--> " + "\"" + c.name  + "\\n1 " + "\"" + c.type + "\n";
+                }
+            }
+            else{
+                attribute += c.name + ":" + c.type + "\n";
+            }
+        }
+        else{
+            if(lastName.length != 0){
+                str + lastName[0] + "-[hidden]>" + c.name;
+                lastName.pop();
+            }else{
+                lastName.push(c.name);
+            }
+            str += elem.name+ " <|-- "+ c.name+"\n";
+        }
+    }
+    return [attribute,str];
+}
+
+module.exports = toPUml;
