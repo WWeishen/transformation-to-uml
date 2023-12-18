3e60a5c361b766306f2d00889d9ba8d166cf65df
weishen <weishenzou@gmail.com>
2023-12-18 16:15:57 +0100

    operator

--- firstTry/ts2uml/lib/__tests__/ts2Uml.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-const ts2Uml = require('../index.js');
-
-describe('ts2Uml', () => {
-  it('has a test', () => {
-    assert(false, 'ts2Uml should have a test');
-  });
-});
--- firstTry/ts2uml/lib/test1.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-const doIt = require('./parser.js');
-doIt("../mylanguage/src/language/generated/ast.ts", {});
