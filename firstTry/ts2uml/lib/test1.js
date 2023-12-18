61700202138ef6f360d26ab9137a2b22cbd695f7
weishen <weishenzou@gmail.com>
2023-12-18 16:19:01 +0100

    operator

---
+++ firstTry/ts2uml/lib/test1.js
@@ -0,0 +1,3 @@
+'use strict';
+const doIt = require('./parser.js');
+doIt("../arithmetics/src/language-server/generated/ast.ts", {});
---
+++ firstTry/ts2uml/lib/toUML/topu.js
@@ -0,0 +1,89 @@
+function isClassName(node,name){
+    return node.children.some(element => element.name === name);
+}
+
+function isCContainer(node, cName) {
+    for (let i = 0; i < node.children.length; i++) {
+        const element = node.children[i];
+        if (element.name === cName) {
+            for (let j = 0; j < element.children.length; j++) {
+                if (element.children[j].name == "$container") {
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+function isContainer(node,elem,c){
+    let result="";
+    node.children.forEach(element => {
+        if (!isCContainer(node, c) && element.name != elem.name) {
+            result = "*";
+        }
+    });
+
+    return result;
+}
+
+function toPUml(node) {//node.name="root"
+    var resultat="";
+    for (let i=0; i < node.children.length; i++){
+        const elem = node.children[i];
+        resultat += "class "+ elem.name + "{\n";
+        let str= selectAttribute(node,elem);
+        resultat += str[0];
+        resultat += "}\n";
+        resultat += str[1];
+    }
+    //console.log(resultat);
+    return resultat;
+}
+
+function selectAttribute(node,elem){
+    var str="";
+    var attribute="";
+    var lastName=[];
+    for (let i=0; i < elem.children.length; i++){
+        const c = elem.children[i];
+        if(c.type != undefined){
+            if(c.type.includes('Array<')){
+                let exp = /<([^>]+)>/;
+                let match = exp.exec(c.type);
+                str += elem.name + isContainer(node,elem,c) + "-->" + "\"" + c.name + "\\n0...*" + "\"" + match[1] +"\n";
+            }
+            else if(c.type.includes('Reference<')){
+                let exp = /<([^>]+)>/;
+                let match = exp.exec(c.type);
+                str += elem.name + "--> " + "\"" + c.name + "\\n0...1" + "\"" + match[1] + "\n";
+            }
+            else if(c.type.includes('Enum<')){
+                let exp = /<([^>]+)>/;
+                let match = exp.exec(c.type);
+                str += elem.name + "-->" + "\"" + c.name + "\"" + match[1] +"\n";
+            }
+            else if(isClassName(node,c.type)){
+                if(c.name != "$container"){
+                    str += elem.name + "*--> " + "\"" + c.name  + "\\n1 " + "\"" + c.type + "\n";
+                }
+            }
+            else{
+                attribute += c.name + ":" + c.type + "\n";
+            }
+        }
+        else{
+            if(lastName.length != 0){
+                str += lastName[0] + "-[hidden]>" + c.name + "\n";
+                lastName.pop();
+            }else{
+                lastName.push(c.name);
+            }
+            str += elem.name + " <|-- " + c.name+"\n";
+        }
+    }
+    return [attribute,str];
+}
+
+
+module.exports = toPUml;
